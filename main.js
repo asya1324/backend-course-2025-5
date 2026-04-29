b700a30b8a2a61e501c18594a08438d6e86a49d1
Asya bc2025-5 <Anastasiia.Samosuieva@lnu.edu.ua>
2026-04-29 21:47:23 +0300

    added put and tested

--- main.js
+++ main.js
@@ -1,27 +1,64 @@
-const {program} = require('commander')
-const fs = require('fs')
-const path = require('path')
-const http = require('http')
+const { program } = require('commander');
+const fs = require('fs');
+const path = require('path');
+const http = require('http');
 
 program
-    .requiredOption("-H, --host <address>")
-    .requiredOption("-p, --port <number>")
-    .requiredOption("-c, --cache <directory>")
+  .requiredOption('-H, --host <address>')
+  .requiredOption('-p, --port <number>')
+  .requiredOption('-c, --cache <directory>');
 
-program.parse()
-const options = program.opts()
+program.parse();
+const options = program.opts();
 
-const cachePath = path.join(__dirname, options.cache)
+const cachePath = path.join(__dirname, options.cache);
 
-if (!fs.existsSync(cachePath)) 
-{
-    fs.mkdirSync(cachePath, { recursive: true })
+if (!fs.existsSync(cachePath)) {
+  fs.mkdirSync(cachePath, { recursive: true });
 }
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' })
-  res.end('Server is running\n')
-})
+const server = http.createServer(async (req, res) => {
+  const code = req.url.slice(1);
+  const filePath = path.join(cachePath, `${code}.jpg`);
+
+  if (!code) {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    return res.end('Not Found');
+  }
+
+  try {
+
+    if (req.method === 'PUT') {
+      const chunks = [];
+
+      req.on('data', (chunk) => {
+        chunks.push(chunk);
+      });
+
+      req.on('end', async () => {
+        const data = Buffer.concat(chunks);
+
+        await fs.promises.writeFile(filePath, data);
+
+        res.writeHead(201, {
+          'Content-Type': 'text/plain'
+        });
+
+        res.end('Created');
+      });
+
+      return;
+    }
+
+
+  } catch (error) {
+    res.writeHead(404, {
+      'Content-Type': 'text/plain'
+    });
+
+    res.end('Not Found');
+  }
+});
 
 server.listen(options.port, options.host, () => {
   console.log(`Server listening on http://${options.host}:${options.port}`);
